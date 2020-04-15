1a5b44f636a03ad20a35b752160a92839c3aecc0
nicola <s1081898@studenti.univpm.it>
2020-04-15 18:04:36 +0200

    aggiunta robba

--- app/src/main/java/com/univpm/futsapp/MainActivity.java
+++ app/src/main/java/com/univpm/futsapp/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity   implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigationView;
@@ -34,12 +34,7 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
 
-        ButtonHandler bh = new ButtonHandler();
-        findViewById(R.id.bGruppi).setOnClickListener(bh);
-        findViewById(R.id.bStorico).setOnClickListener(bh);
-        findViewById(R.id.bIdeeFuture).setOnClickListener(bh);
-        findViewById(R.id.bNuovaPartita).setOnClickListener(bh);
-        findViewById(R.id.bProssimoEvento).setOnClickListener(bh);
+
 
     }
 
@@ -47,7 +42,7 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.profile:
-                Toast.makeText(MainActivity.this, "Profile Selected", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Profile Selected", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.contact:
                 Toast.makeText(MainActivity.this, "Contact us Selected", Toast.LENGTH_SHORT).show();
@@ -57,40 +52,11 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
                 break;
             case R.id.logout:
                 Toast.makeText(MainActivity.this, "Log Out Selected", Toast.LENGTH_SHORT).show();
-                break;
-        }
+                break;}
 
 
         return false;
     }
 
-    private class ButtonHandler implements View.OnClickListener {
 
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.bGruppi:
-                    show("visualizzo i gruppi");
-                    break;
-                case R.id.bStorico:
-                    show("visualizzo lo storico");
-                    break;
-                case R.id.bIdeeFuture:
-                    show("cosa ci riserva il futuro");
-                    break;
-                case R.id.bNuovaPartita:
-                    show("creiamo una nuova partita");
-                    break;
-                case R.id.bProssimoEvento:
-                    show("il prossimo evento è...");
-                    break;
-                default:
-                    show("non dovrebbe succedere");
-            }
-        }
-    }
-
-    void show(String s) {
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
-    }
 }
