ba3e606c6e783216336e2c6b8be51928e914ea39
Dell <davide.narcisi98@gmail.com>
2020-04-15 17:31:50 +0200

    aggiunta classe buttonhandler per main activity

--- app/src/main/java/com/univpm/futsapp/MainActivity.java
+++ app/src/main/java/com/univpm/futsapp/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
 
+        ButtonHandler bh = new ButtonHandler();
+        findViewById(R.id.bGruppi).setOnClickListener(bh);
+        findViewById(R.id.bStorico).setOnClickListener(bh);
+        findViewById(R.id.bIdeeFuture).setOnClickListener(bh);
+        findViewById(R.id.bNuovaPartita).setOnClickListener(bh);
+        findViewById(R.id.bProssimoEvento).setOnClickListener(bh);
 
     }
 
@@ -40,7 +47,7 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.profile:
-                Toast.makeText(MainActivity.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Profile Selected", Toast.LENGTH_LONG).show();
                 break;
             case R.id.contact:
                 Toast.makeText(MainActivity.this, "Contact us Selected", Toast.LENGTH_SHORT).show();
@@ -50,9 +57,40 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
                 break;
             case R.id.logout:
                 Toast.makeText(MainActivity.this, "Log Out Selected", Toast.LENGTH_SHORT).show();
-                break;}
+                break;
+        }
 
 
         return false;
+    }
+
+    private class ButtonHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.bGruppi:
+                    show("visualizzo i gruppi");
+                    break;
+                case R.id.bStorico:
+                    show("visualizzo lo storico");
+                    break;
+                case R.id.bIdeeFuture:
+                    show("cosa ci riserva il futuro");
+                    break;
+                case R.id.bNuovaPartita:
+                    show("creiamo una nuova partita");
+                    break;
+                case R.id.bProssimoEvento:
+                    show("il prossimo evento è...");
+                    break;
+                default:
+                    show("non dovrebbe succedere");
+            }
+        }
+    }
+
+    void show(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
