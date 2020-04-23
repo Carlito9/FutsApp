e1ac33b52e9cb002f4a9d073048c743e65313eac
mo-98 <mo-98@hotmail.it>
2020-04-23 16:25:01 +0200

    Btn_Salvataggio

--- app/src/main/java/com/univpm/futsapp/NewGameActivity.java
+++ app/src/main/java/com/univpm/futsapp/NewGameActivity.java
@@ -5,22 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 
 public class NewGameActivity extends AppCompatActivity   {
-
     TextView tv;
+    EditText Name,Luogo,Costo;
+    TextView Data;
+    TimePicker Orario;
+    Button btn_conferma;
     Calendar mCurrentDate;
     int day,month,year;
+    FirebaseFirestore db;
+    FirebaseAuth col;
+    Match match;
+    FirebaseUser currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
         ButtonHandler bh = new ButtonHandler();
+
+        Name=(EditText)findViewById(R.id.Name);
+        Luogo=(EditText)findViewById(R.id.Luogo);
+        Costo=(EditText)findViewById(R.id.Costo);
+        Data=(TextView)findViewById(R.id.Data);
+        Orario=(TimePicker)findViewById(R.id.Orario);
+        System.out.println(Orario);
+        col=FirebaseAuth.getInstance();
+        currentUser = col.getCurrentUser();
+        db= FirebaseFirestore.getInstance();
+
+
+
+
         findViewById(R.id.goback).setOnClickListener(bh);
         findViewById(R.id.btn_conferma).setOnClickListener(bh);
 
@@ -62,7 +93,19 @@ public class NewGameActivity extends AppCompatActivity   {
                     finish();
                     break;
                 case R.id.btn_conferma:
-                    show("Ancora da creare");
+                {
+                    int costo = Integer.parseInt(Costo.getText().toString().trim());
+                    String data = Data.getText().toString().trim();
+                    String name = Name.getText().toString().trim();
+                    String luogo = Luogo.getText().toString().trim();
+
+
+                    match = new Match(name,costo,luogo,data);
+
+                    db.collection("partita").document(currentUser.getUid()).set(match);
+                    Toast.makeText(NewGameActivity.this,"Salvato",Toast.LENGTH_LONG).show();
+                }
+               break;
             }
         }
     }
