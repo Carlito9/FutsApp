4749120f7ce017d15e3693706e60fe0581f629b1
mo-98 <mo-98@hotmail.it>
2020-08-18 11:15:27 +0200

    Search

--- app/src/main/java/com/univpm/futsapp/FragmentSearch.java
+++ app/src/main/java/com/univpm/futsapp/FragmentSearch.java
@@ -1,11 +1,15 @@
 package com.univpm.futsapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,27 +18,61 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.univpm.futsapp.NewGame.DataList;
 import com.univpm.futsapp.NewGame.NewGameActivity;
+
+import java.util.ArrayList;
 
 public class FragmentSearch extends Fragment {
     ListView lv;
     View view;
     FirebaseListAdapter adapter;
+    ImageButton searchbutton;
+    EditText searchtext;
+
+    RecyclerView giocatori;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_search,container,false);
 
-        RecyclerView giocatori = (RecyclerView) view.findViewById(R.id.Rubrica);
-        giocatori.setHasFixedSize(true);
-        RecyclerView.LayoutManager lManager = new LinearLayoutManager(view.getContext());
-        RecyclerView.Adapter mAdapter = new ListaRubrica(NewGameActivity.amici,view.getContext());
-        giocatori.setAdapter(mAdapter);
-        giocatori.setLayoutManager(lManager);
+        giocatori = (RecyclerView) view.findViewById(R.id.Rubrica);
+        Crearecycle (MainActivity.players);
+        searchbutton = view.findViewById(R.id.searchbutton);
+        searchtext = view.findViewById(R.id.searchtext);
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                   Filtrarisultati( searchtext.getText().toString().trim());
+                } catch (NullPointerException|IllegalArgumentException e ){
+                    Crearecycle(MainActivity.players); 
+                }
+
+            }
+        });
         return view;
         }
+
+    private void Crearecycle(DataList[] players) {
+        giocatori.setHasFixedSize(true);
+        RecyclerView.LayoutManager lManager = new LinearLayoutManager(view.getContext());
+        RecyclerView.Adapter mAdapter = new ListaRubrica(players,view.getContext());
+        giocatori.setAdapter(mAdapter);
+        giocatori.setLayoutManager(lManager);
+    }
+
+    private void Filtrarisultati(String nome) {
+        ArrayList <DataList> giocatori = new ArrayList<>();
+        for (DataList d : MainActivity.players) {
+            if (d.getUsername().contains(nome))
+                giocatori.add(d);
+        }
+         Crearecycle(giocatori.toArray(new DataList[0]) );
+    }
+
 
 }
